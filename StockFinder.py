f7bdfdcb14f8a6c5339365ca76d70b3ed6462e3c
J0s <joshi.karthik@yahoo.com>
2020-12-03 23:54:23 +0530

    stock finder python script

---
+++ StockFinder.py
@@ -0,0 +1,47 @@
+#Script to find any stock which is not usually available
+#also can be modified to buy any item during flash sale
+
+import requests
+import logging
+import re
+from playsound import playsound
+from threading import Thread
+
+#setting logging basic info
+logging.basicConfig(level = logging.INFO, filename = 'findCards.log', filemode = 'a',format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+#add product links here
+urls=["https://www.newegg.com/evga-geforce-rtx-3080-10g-p5-3897-kr/p/N82E16814487518",
+      "https://www.newegg.com/evga-geforce-rtx-3080-10g-p5-3885-kr/p/N82E16814487520",
+      "https://www.newegg.com/pny-geforce-rtx-3080-vcg308010tfxmpb/p/N82E16814133810",
+      "https://www.newegg.com/evga-geforce-rtx-3080-10g-p5-3885-kr/p/N82E16814487520",
+      "https://www.newegg.com/asus-geforce-rtx-3080-tuf-rtx3080-10g-gaming/p/N82E16814126453",
+      "https://www.bhphotovideo.com/c/product/1595780-REG/pny_technologies_vcg308010tfxmpb_geforce_rtx_3080_10gb.html",
+      ]
+
+
+#stock text to validate
+outOfStockTextList = ["OUT OF STOCK","Notify When Available"]
+inStockTextList = ["In Stock","Add to Cart"]
+
+#function to play music when stock found
+def playFoundMusic():
+   playsound("https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3")
+
+#loop over all urls and check if stock is present
+for j in range(100):
+    for i in range(len(urls)):
+        
+        x = re.search("https?://([A-Za-z_0-9.-]+).*", urls[i])
+        r = requests.get(urls[i])
+        res = r.text.encode("utf-8")
+        if(r.status_code != 200):
+            logging.error ("ERROR! STATUS: "+r.status_code+" ["+x.group(1)+"]"+urls[i])
+        elif not(any(outOfStockText in res for outOfStockText in outOfStockTextList)):
+            logging.info ("FOUND: ["+x.group(1)+"]"+urls[i])
+            Thread(target=playFoundMusic).start()
+        else:
+            logging.info ("NOT FOUND: ["+x.group(1)+"]"+urls[i])
+
+
+
